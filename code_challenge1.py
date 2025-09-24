d6025f1a31f9522a2e0ae3456ff20659ae6d690c
Marianne Pereda <mariannepereda10@gmail.com>
2025-09-24 18:59:13 +0800

    Update and rename codechallenge1.py to code_challenge1.py

--- code_challenge1.py
+++ code_challenge1.py
@@ -1,2 +1,2 @@
-name = input("What is you name? ")
+name = input("What is your name? ")
 print("\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t*\n\t\t\t\t*\t\t\t\t*\n\t\t\t*\t\t\t\t\t\t*\n\t\t*\t\t\t\tHi\t\t\t\t*\n\t\t\t*\t\t\tMarianne\t\t*\n\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t*")
